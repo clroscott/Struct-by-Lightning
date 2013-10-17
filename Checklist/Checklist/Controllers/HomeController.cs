e90771d34e95da9ea4504d6861fd915710336a1c
clroscott <clroscott@gmail.com>
2013-10-16 22:11:50 -0700

    Clayton: deleted some of the default views and modified controllers based on that

--- Checklist/Checklist/Controllers/HomeController.cs
+++ Checklist/Checklist/Controllers/HomeController.cs
@@ -8,27 +8,6 @@ namespace Checklist.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         public ActionResult LocationList()
         {
             ViewBag.Message = "List of Locations.";
